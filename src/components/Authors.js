c611f6b4ce0d03b32457e6f72150d970ca845d60
aarekr <aare212@hotmail.com>
2023-03-29 14:37:09 +0300

    Tehtävä 8.12 kirjailijan syntymävuosi advanced

--- src/components/Authors.js
+++ src/components/Authors.js
@@ -3,61 +3,58 @@ import { useMutation } from "@apollo/client"
 import { EDIT_AUTHOR_YEAR } from "./queries"
 
 const Authors = (props) => {    
-    const [ name, setName ] = useState('')
-    const [ born, setYear ] = useState(1000)
-    const [ changeYear ] = useMutation(EDIT_AUTHOR_YEAR)
+  const [ name, setName ] = useState('')
+  const [ born, setYear ] = useState('')
+  const [ changeYear ] = useMutation(EDIT_AUTHOR_YEAR)
 
-    if (!props.show) { return null }
+  if (!props.show) { return null }
 
-    const submit = (event) => {
-      event.preventDefault()
-      const setBornTo = born
-      changeYear({ variables: { name, setBornTo } })
-      setName('')
-      setYear('')
-    }
+  const submit = (event) => {
+    event.preventDefault()
+    const setBornTo = born
+    changeYear({ variables: { name, setBornTo } })
+    setName('')
+    setYear('')
+  }
 
-    if (props.authors) {
-      return (
-        <div>
-          <h2>Authors</h2>
-          <table>
-            <tbody>
-              <tr>
-                <th>name</th>
-                <th>born</th>
-                <th>books</th>
+  if (props.authors) {
+    return (
+      <div>
+        <h2>Authors</h2>
+        <table>
+          <tbody>
+            <tr>
+              <th>name</th>
+              <th>born</th>
+              <th>books</th>
+            </tr>
+            {props.authors.map((a) => (
+              <tr key={a.name}>
+                <td>{a.name}</td>
+                <td>{a.born}</td>
+                <td>{a.bookCount}</td>
               </tr>
-              {props.authors.map((a) => (
-                <tr key={a.name}>
-                  <td>{a.name}</td>
-                  <td>{a.born}</td>
-                  <td>{a.bookCount}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+            ))}
+          </tbody>
+        </table>
 
-          <h3>Set birthyear</h3>
-          <form onSubmit={submit}>
-            <div>
-              Name: <input value={name} onChange={({ target }) => setName(target.value)} />
-            </div>
-            <div>
-              Born: <input value={Number(born)} 
-                           onChange={({ target }) => setYear(Number(target.value))} />
-            </div>
-            <button type="submit">update author</button>
-          </form>
-
-          <form>
+        <h3>Set birthyear</h3>
+        <form onSubmit={submit}>
+          <div>
+            Name: 
             <select onChange={({ target }) => setName(target.value)}>
               {props.authors.map(a => <option key={a.name}>{a.name}</option>)}
             </select>
-          </form>
-        </div>
-      )
-    }
+          </div>
+          <div>
+            Born:
+            <input value={born} onChange={({ target }) => setYear(Number(target.value))} />
+          </div>
+          <button type="submit">update author</button>
+        </form>
+      </div>
+    )
   }
+}
 
 export default Authors
